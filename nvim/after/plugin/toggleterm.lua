fcb21c1ad3085e7b4fd499d14d86ab35675d2b16
Sushil Chhetri <sushil.chhetri@play.co>
2022-03-10 14:52:44 +0530

    adding keymappings for toggle term

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -25,3 +25,36 @@ toggleterm.setup({
     },
   },
 })
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
+local node = Terminal:new({ cmd = "node", hidden = true })
+
+function _NODE_TOGGLE()
+  node:toggle()
+end
+
+local gtop = Terminal:new({ cmd = "gtop", hidden = true })
+
+function _GTOP_TOGGLE()
+  gtop:toggle()
+end
+
+local wk_status_ok, wk = pcall(require, "which-key")
+if not wk_status_ok then
+  return
+end
+
+wk.register({
+  t = {
+    name = "ToggleTerm",
+    n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node Terminal" },
+    g = { "<cmd>lua _LAZYGIT_TOGGLE()<cr>", "Lazygit Terminal" },
+    h = { "<cmd>lua _GTOP_TOGGLE()<cr>", "Gtop Terminal" },
+  }
+}, {prefix = "<leader>"})
