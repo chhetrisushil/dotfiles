0f8cdd0e2ab4e72410039b961350e2206f68a9fa
Sushil Chhetri <sushil.chhetri@play.co>
2022-03-22 12:24:39 +0530

    adding vairbale to look for repos and a key binding

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,9 @@
 -- load extensions for telescope
 local telescope = require 'telescope'
 
+-- set this variable if you want to change the repo listing path. Use `:lua workspace_loc='<path you want to look for repos>'`
+workspace_loc = '~/workspace'
+
 telescope.load_extension('fzf')
 telescope.load_extension('file_browser')
 telescope.load_extension('repo')
@@ -19,5 +22,6 @@ wk.register({
         b = {":lua require'telescope.builtin'.buffers()<cr>", "Find buffers"},
         h = {":lua require'telescope.builtin'.help_tags()<cr>", "Help tags"},
     },
-    ["<C-p>"] = {":lua require'telescope.builtin'.find_files()<cr>", "Find files"}
+    ["<C-p>"] = {":lua require'telescope.builtin'.find_files()<cr>", "Find files"},
+    ["<leader>lr"] = {function () require'telescope'.extensions.repo.list{search_dirs={workspace_loc}} end, "Open repo list"}
 }, {nnoremap = true, silent = true})
