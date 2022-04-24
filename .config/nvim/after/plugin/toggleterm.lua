d43ed45f12bc3f2b973ad2a16fb5affb9a08c9f9
Sushil Chhetri <chhetrisushil@gmail.com>
2022-04-24 11:14:23 +0530

    some mappings inside neovim terminal

--- .config/nvim/after/plugin/toggleterm.lua
+++ .config/nvim/after/plugin/toggleterm.lua
@@ -26,6 +26,15 @@ toggleterm.setup({
   },
 })
 
+-- set custom mappings for terminal
+function _G.set_terminal_keymaps()
+  local opts = {noremap = true }
+
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-J>', [[<C-\><C-n>]], opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
