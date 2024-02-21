27421d94fe68d22212f9ed39534b5ba2326998af
Sushil Chhetri <chhetrisushil@gmail.com>
2024-02-21 13:01:03 +0530

    xmonad: Resolving deprecation warning.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -43,7 +43,7 @@ import XMonad.Layout.LayoutCombinators
 import XMonad.Layout.ShowWName
 import XMonad.Layout.Spacing
 import XMonad.Layout.PerScreen
-import XMonad.Layout.Named
+import XMonad.Layout.Renamed
 import XMonad.Layout.SubLayouts
 import XMonad.Layout.WindowNavigation
 import XMonad.Layout.BoringWindows
