2637ae8d6aa4c6bf0e20286f3d83bb474ec77fad
Sushil Chhetri <chhetrisushil@gmailcom>
2024-12-23 07:54:22 +0530

    nvimd: markdown and neorg plugin

---
+++ .config/nvim/lua/plugins/markdown-config.lua
@@ -0,0 +1,5 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  opts = {},
+}
---
+++ .config/nvim/lua/plugins/neorg-config.lua
@@ -0,0 +1,6 @@
+return {
+	"nvim-neorg/neorg",
+	lazy = false,
+	version = "*", -- Pin Neorg to the latest stable release
+	config = true,
+}
