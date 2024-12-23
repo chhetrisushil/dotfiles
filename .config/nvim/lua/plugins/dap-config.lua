892465c9695ede466f7862033f49ed34d8abfa71
Sushil Chhetri <chhetrisushil@gmailcom>
2024-12-23 09:39:13 +0530

    nvim: debugger plugin

---
+++ .config/nvim/lua/plugins/dap-config.lua
@@ -0,0 +1,100 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"mxsdev/nvim-dap-vscode-js",
+		"leoluz/nvim-dap-go",
+		{
+			"microsoft/vscode-js-debug",
+			opt = true,
+			run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+		},
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local wk = require("which-key")
+
+		dapui.setup()
+		require("dap-go").setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- Keymaps
+		wk.add({
+			{ "<leader>dt", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
+			{ "<leader>dc", dap.continue, desc = "Continue" },
+		})
+
+		require("dap-vscode-js").setup({
+			-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+			-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+			-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+			adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+			-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+			-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+			-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+		})
+
+		for _, language in ipairs({ "typescript", "javascript" }) do
+			require("dap").configurations[language] = {
+				{
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Launch file",
+						program = "${file}",
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "attach",
+						name = "Attach",
+						processId = require("dap.utils").pick_process,
+						cwd = "${workspaceFolder}",
+					},
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Debug Jest Tests",
+						-- trace = true, -- include debugger info
+						runtimeExecutable = "node",
+						runtimeArgs = {
+							"./node_modules/jest/bin/jest.js",
+							"--runInBand",
+						},
+						rootPath = "${workspaceFolder}",
+						cwd = "${workspaceFolder}",
+						console = "integratedTerminal",
+						internalConsoleOptions = "neverOpen",
+					},
+					{
+						type = "pwa-node",
+						request = "launch",
+						name = "Debug Mocha Tests",
+						-- trace = true, -- include debugger info
+						runtimeExecutable = "node",
+						runtimeArgs = {
+							"./node_modules/mocha/bin/mocha.js",
+						},
+						rootPath = "${workspaceFolder}",
+						cwd = "${workspaceFolder}",
+						console = "integratedTerminal",
+						internalConsoleOptions = "neverOpen",
+					},
+				},
+			}
+		end
+	end,
+}
