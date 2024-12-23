8b94a4c4314f5165b2793fafa4a1e20828003226
Sushil Chhetri <chhetrisushil@gmailcom>
2024-12-23 08:34:18 +0530

    nvim: markdown preview

--- .config/nvim/lua/plugins/markdown-config.lua
+++ .config/nvim/lua/plugins/markdown-config.lua
@@ -1,5 +1,5 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
   opts = {},
 }
--- .config/nvim/lua/plugins/neorg-config.lua
+++ .config/nvim/lua/plugins/neorg-config.lua
@@ -2,5 +2,24 @@ return {
 	"nvim-neorg/neorg",
 	lazy = false,
 	version = "*", -- Pin Neorg to the latest stable release
-	config = true,
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.summary"] = {},
+        ["core.completion"] = { config = { engine = "nvim-cmp" } },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              work = "~/Documents/Neorg/Work",
+              personal = "~/Documents/Neorg/Personal",
+            },
+          },
+        },
+      },
+    })
+  end
 }
