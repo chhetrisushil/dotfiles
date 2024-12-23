714c7a82e2f1a14fa2c85617d9ce399cab249d7f
Sushil Chhetri <chhetrisushil@gmail.com>
2024-12-23 13:51:01 +0530

    nvim: transparent background

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -4,6 +4,10 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      transparent_background = true,
+    })
+
     vim.cmd.colorscheme "catppuccin-mocha"
   end
 }
