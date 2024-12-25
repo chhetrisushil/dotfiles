92b104d7e9100b5476b15b5af996a53327fa7844
Sushil Chhetri <chhetrisushil@gmailcom>
2024-12-25 16:28:00 +0530

    nvim: lsp config for bash and go

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -3,24 +3,44 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
+          "biome",
           "lua_ls",
           "ts_ls",
           "denols",
+          "bashls",
+          "gopls",
         },
       })
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     dependencies = {
       "folke/which-key.nvim",
+    },
+    opts = {
+      biome = {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "json",
+          "jsonc",
+          "typescript",
+          "typescript.tsx",
+          "typescriptreact",
+          "astro",
+          "svelte",
+          "vue",
+          "css",
+        },
+      },
     },
     config = function()
       local lspconfig = require("lspconfig")
@@ -28,16 +48,23 @@ return {
 
       -- keymaps
       wk.add({
-        { "<leader>D", vim.lsp.buf.type_definition, desc = "Type Definition", remap = false },
-        { "<leader>ca", vim.lsp.buf.code_action, desc = "Code action", remap = false },
+        { "<leader>D",  vim.lsp.buf.type_definition,      desc = "Type Definition",      remap = false },
+        { "<leader>ca", vim.lsp.buf.code_action,          desc = "Code action",          remap = false },
         -- { "<leader>f", function() vim.lsp.buf.format({ async = true }) end, desc = "Formatting", remap = false },
-        { "<leader>j", vim.diagnostic.goto_next, desc = "Goto Next Diagnostic", remap = false },
-        { "<leader>k", vim.diagnostic.goto_prev, desc = "Goto Prev Diagnostic", remap = false },
-        { "<leader>q", vim.diagnostic.setloclist, desc = "Loc list", remap = false },
-        { "<leader>rn", vim.lsp.buf.rename, desc = "Rename variable", remap = false },
-        { "<leader>w", group = "Workspace", remap = false },
-        { "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add workspace", remap = false },
-        { "<leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, desc = "List workspace", remap = false },
+        { "<leader>j",  vim.diagnostic.goto_next,         desc = "Goto Next Diagnostic", remap = false },
+        { "<leader>k",  vim.diagnostic.goto_prev,         desc = "Goto Prev Diagnostic", remap = false },
+        { "<leader>q",  vim.diagnostic.setloclist,        desc = "Loc list",             remap = false },
+        { "<leader>rn", vim.lsp.buf.rename,               desc = "Rename variable",      remap = false },
+        { "<leader>w",  group = "Workspace",              remap = false },
+        { "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add workspace",        remap = false },
+        {
+          "<leader>wl",
+          function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end,
+          desc = "List workspace",
+          remap = false,
+        },
         { "<leader>wr", vim.lsp.buf.remove_workspace_folder, desc = "Remove workspace", remap = false },
       })
 
@@ -47,6 +74,12 @@ return {
       -- Javascrip/Typescript lsp config
       lspconfig.denols.setup({})
       lspconfig.ts_ls.setup({})
-    end
+
+      -- Go lsp config
+      lspconfig.gopls.setup({})
+
+      -- Bash lsp config
+      lspconfig.bashls.setup({})
+    end,
   },
 }
