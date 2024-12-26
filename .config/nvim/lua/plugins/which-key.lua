ce7784a1f0c791c4e8e7c13b52b2c82f3f93f5cd
Sushil Chhetri <chhetrisushil@gmail.com>
2024-12-26 11:39:04 +0530

    nvim: `hipatterns` plugin setup to highlight colors

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -16,10 +16,27 @@ return {
     },
   },
   config = function()
-    require("which-key").add({
-      { "<leader>b", group = "Buffer commands", desc = "Buffer commands", remap = false },
-      { "<leader>bd", "<cmd>bd<CR>", desc = "Delete current buffer", remap = false },
-      { "<C-l>", "<cmd>nohl<CR>", desc = "No highlight", remap = false, mode = "n" },
+    local hipatterns = require("mini.hipatterns")
+
+    -- Setup
+    hipatterns.setup({
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
     })
-  end
+
+    -- Keymaps
+    require("which-key").add({
+      { "<leader>b",  group = "Buffer commands", desc = "Buffer commands",       remap = false },
+      { "<leader>bd", "<cmd>bd<CR>",             desc = "Delete current buffer", remap = false },
+      { "<C-l>",      "<cmd>nohl<CR>",           desc = "No highlight",          remap = false, mode = "n" },
+    })
+  end,
 }
