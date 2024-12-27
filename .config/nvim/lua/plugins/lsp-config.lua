afd01e1c20f4d26cd66b592c02a1c82a62fa7630
Sushil Chhetri <chhetrisushil@gmailcom>
2024-12-27 21:00:27 +0530

    nvim: lsp-config for `nginx`

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,106 +1,104 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "biome",
-          "lua_ls",
-          "ts_ls",
-          "denols",
-          "bashls",
-          "gopls",
-          "html",
-          "emmet_language_server",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "folke/which-key.nvim",
-    },
-    opts = {
-      biome = {
-        filetypes = {
-          "javascript",
-          "javascriptreact",
-          "json",
-          "jsonc",
-          "typescript",
-          "typescript.tsx",
-          "typescriptreact",
-          "astro",
-          "svelte",
-          "vue",
-          "css",
-        },
-      },
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
-      local wk = require("which-key")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"biome",
+					"lua_ls",
+					"ts_ls",
+					"denols",
+					"bashls",
+					"gopls",
+					"html",
+					"emmet_language_server",
+					"nginx_language_server",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			biome = {
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"json",
+					"jsonc",
+					"typescript",
+					"typescript.tsx",
+					"typescriptreact",
+					"astro",
+					"svelte",
+					"vue",
+					"css",
+				},
+			},
+		},
+		keys = {
+			{ "gd", vim.lsp.buf.type_definition, desc = "Type Definition", remap = false },
+			{ "<leader>ca", vim.lsp.buf.code_action, desc = "Code action", remap = false },
+			-- { "<leader>f", function() vim.lsp.buf.format({ async = true }) end, desc = "Formatting", remap = false },
+			{ "<leader>j", vim.diagnostic.goto_next, desc = "Goto Next Diagnostic", remap = false },
+			{ "<leader>k", vim.diagnostic.goto_prev, desc = "Goto Prev Diagnostic", remap = false },
+			{ "<leader>q", vim.diagnostic.setloclist, desc = "Loc list", remap = false },
+			{ "<leader>rn", vim.lsp.buf.rename, desc = "Rename variable", remap = false },
+			{ "<leader>w", group = "Workspace", remap = false },
+			{ "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add workspace", remap = false },
+			{
+				"<leader>wl",
+				function()
+					print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+				end,
+				desc = "List workspace",
+				remap = false,
+			},
+			{ "<leader>wr", vim.lsp.buf.remove_workspace_folder, desc = "Remove workspace", remap = false },
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      -- keymaps
-      wk.add({
-        { "<leader>D",  vim.lsp.buf.type_definition,      desc = "Type Definition",      remap = false },
-        { "<leader>ca", vim.lsp.buf.code_action,          desc = "Code action",          remap = false },
-        -- { "<leader>f", function() vim.lsp.buf.format({ async = true }) end, desc = "Formatting", remap = false },
-        { "<leader>j",  vim.diagnostic.goto_next,         desc = "Goto Next Diagnostic", remap = false },
-        { "<leader>k",  vim.diagnostic.goto_prev,         desc = "Goto Prev Diagnostic", remap = false },
-        { "<leader>q",  vim.diagnostic.setloclist,        desc = "Loc list",             remap = false },
-        { "<leader>rn", vim.lsp.buf.rename,               desc = "Rename variable",      remap = false },
-        { "<leader>w",  group = "Workspace",              remap = false },
-        { "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add workspace",        remap = false },
-        {
-          "<leader>wl",
-          function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end,
-          desc = "List workspace",
-          remap = false,
-        },
-        { "<leader>wr", vim.lsp.buf.remove_workspace_folder, desc = "Remove workspace", remap = false },
-      })
+			-- lua lsp config
+			lspconfig.lua_ls.setup({})
 
-      -- lua lsp config
-      lspconfig.lua_ls.setup({})
+			-- Javascrip/Typescript lsp config
+			lspconfig.denols.setup({})
+			lspconfig.ts_ls.setup({})
 
-      -- Javascrip/Typescript lsp config
-      lspconfig.denols.setup({})
-      lspconfig.ts_ls.setup({})
+			-- Go lsp config
+			lspconfig.gopls.setup({})
 
-      -- Go lsp config
-      lspconfig.gopls.setup({})
+			-- Bash lsp config
+			lspconfig.bashls.setup({})
 
-      -- Bash lsp config
-      lspconfig.bashls.setup({})
+			-- HTML lsp config
+			lspconfig.html.setup({})
 
-      -- HTML lsp config
-      lspconfig.html.setup({})
+			-- Emmet lsp config
+			lspconfig.emmet_language_server.setup({
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"less",
+					"sass",
+					"scss",
+					"pug",
+					"typescriptreact",
+				},
+			})
 
-      -- Emmet lsp config
-      lspconfig.emmet_language_server.setup({
-        filetypes = {
-          "css",
-          "eruby",
-          "html",
-          "javascript",
-          "javascriptreact",
-          "less",
-          "sass",
-          "scss",
-          "pug",
-          "typescriptreact",
-        },
-      })
-    end,
-  },
+			-- Nginx lsp config
+			lspconfig.nginx_language_server.setup({})
+		end,
+	},
 }
