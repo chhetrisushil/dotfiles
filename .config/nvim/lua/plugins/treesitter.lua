88b2efc63c7e91e644cb351768a4c898eaf728ba
Sushil Chhetri <chhetrisushil@gmailcom>
2025-01-05 21:32:38 +0530

    nvim: adding text object support to treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "nvim-treesitter/nvim-treesitter-context",
+  },
   build = ":TSUpdate",
   config = function()
     local configs = require("nvim-treesitter.configs")
@@ -36,9 +40,42 @@ return {
       autotag = {
         enable = true,
       },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<localleader>vs",
+          node_incremental = "<localleader>vs",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["acc"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["a="] = "@assignment.outer",
+            ["i="] = "@assignment.inner",
+            ["alh"] = "@assignment.lhs",
+            ["arh"] = "@assignment.rhs",
+            ["aro"] = "@pattern.outer",
+            ["ari"] = "@pattern.inner",
+            ["aco"] = "@conditional.outer",
+            ["aci"] = "@conditional.inner",
+            ["alo"] = "@loop.outer",
+            ["ali"] = "@loop.inner",
+          },
+        },
+      },
     })
 
-    local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
     parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-  end
+  end,
 }
