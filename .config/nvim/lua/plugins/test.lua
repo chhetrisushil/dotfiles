8d169c6cd1da8701fb3715bef6bb28481ade1d41
Sushil Chhetri <chhetrisushil@gmail.com>
2025-01-16 20:59:26 +0530

    nvim: mapping test keymaps to localleader

--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -13,21 +13,21 @@ return {
   },
   keys = {
     {
-      "<leader>tl",
+      "<localleader>tl",
       function()
         require("neotest").run.run_last()
       end,
       desc = "Run Last Test",
     },
     {
-      "<leader>tL",
+      "<localleader>tL",
       function()
         require("neotest").run.run_last({ strategy = "dap" })
       end,
       desc = "Debug Last Test",
     },
     {
-      "<leader>tw",
+      "<localleader>tw",
       "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
       desc = "Run Watch",
     },
