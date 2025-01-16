9ec265339c39ef3365aa22e721ec4bf4bed2b928
Sushil Chhetri <chhetrisushil@gmail.com>
2025-01-16 20:42:51 +0530

    nvim: neotest plugin

---
+++ .config/nvim/lua/plugins/test.lua
@@ -0,0 +1,69 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    -- test runners
+    "nvim-neotest/neotest-jest",
+    "marilari88/neotest-vitest",
+    "nvim-neotest/neotest-go",
+    "nvim-neotest/neotest-pythond",
+  },
+  keys = {
+    {
+      "<leader>tl",
+      function()
+        require("neotest").run.run_last()
+      end,
+      desc = "Run Last Test",
+    },
+    {
+      "<leader>tL",
+      function()
+        require("neotest").run.run_last({ strategy = "dap" })
+      end,
+      desc = "Debug Last Test",
+    },
+    {
+      "<leader>tw",
+      "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+      desc = "Run Watch",
+    },
+  },
+  config = function()
+    -- get neotest namespace (api call creates or returns namespace)
+    local neotest_ns = vim.api.nvim_create_namespace("neotest")
+    vim.diagnostic.config({
+      virtual_text = {
+        format = function(diagnostic)
+          local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+          return message
+        end,
+      },
+    }, neotest_ns)
+
+    require("neotest").setup({
+      adapters = {
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          jestConfigFile = "custom.jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        }),
+        require("neotest-vitest"),
+        require("neotest-go")({
+          experimental = {
+            test_table = true,
+          },
+          args = { "-count=1", "-timeout=60s" },
+          recursive_run = true,
+        }),
+        require("neotest-python"),
+      },
+    })
+  end,
+}
