d4275231bc0c23dc5e7bcea316a477d7cfd5bca9
Sushil Chhetri <chhetrisushil@gmailcom>
2025-01-17 01:42:11 +0530

    nvim: icon and color update for nvim-dap

--- .config/nvim/lua/plugins/dap-config.lua
+++ .config/nvim/lua/plugins/dap-config.lua
@@ -1,112 +1,127 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"mxsdev/nvim-dap-vscode-js",
-		"leoluz/nvim-dap-go",
-		"theHamsta/nvim-dap-virtual-text",
-		"nvim-neotest/nvim-nio",
-		{
-			"microsoft/vscode-js-debug",
-			opt = true,
-			run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-		},
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
-		local wk = require("which-key")
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "mxsdev/nvim-dap-vscode-js",
+    "leoluz/nvim-dap-go",
+    "theHamsta/nvim-dap-virtual-text",
+    "nvim-neotest/nvim-nio",
+    {
+      "microsoft/vscode-js-debug",
+      opt = true,
+      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+    },
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+    local wk = require("which-key")
 
-		-- Setup
-		dapui.setup()
-		require("dap-go").setup()
-		require("nvim-dap-virtual-text").setup()
+    -- breakpoint icons and colors
+    vim.cmd("hi DapColor guifg=#fa4848")
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapColor", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapBreakpointCondition", { text = "c", texthl = "DapColor", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapColor", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapBreakpointRejected", { text = "R", texthl = "DapColor", linehl = "", numhl = "" })
+    vim.fn.sign_define("DapLogPoint", { text = "L", texthl = "DapColor", linehl = "", numhl = "" })
 
-		-- Listeners
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    -- Setup
+    dapui.setup()
+    require("dap-go").setup()
+    require("nvim-dap-virtual-text").setup()
 
-		-- Keymaps
-		wk.add({
-			{ "<leader>dt", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
-			{ "<leader>dc", dap.continue, desc = "Continue" },
-			{
-				"<leader>d?",
-				function()
-					dapui.eval(nil, { enter = true })
-				end,
-				desc = "Eval",
-			},
-		})
+    -- Listeners
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		require("dap-vscode-js").setup({
-			-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-			-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-			-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-			adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-			-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-			-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-			-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-		})
+    -- Keymaps
+    wk.add({
+      { "<leader>dt", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
+      {
+        "<leader>dT",
+        function()
+          dap.toggle_breakpoint(vim.fn.input("Breakpoint condition: "))
+        end,
+        desc = "Toggle Conditional Breakpoint",
+      },
+      { "<leader>dc", dap.continue,          desc = "Continue" },
+      {
+        "<leader>d?",
+        function()
+          dapui.eval(nil, { enter = true })
+        end,
+        desc = "Eval",
+      },
+    })
 
-		for _, language in ipairs({ "typescript", "javascript" }) do
-			require("dap").configurations[language] = {
-				{
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Launch file",
-						program = "${file}",
-						cwd = "${workspaceFolder}",
-					},
-					{
-						type = "pwa-node",
-						request = "attach",
-						name = "Attach",
-						processId = require("dap.utils").pick_process,
-						cwd = "${workspaceFolder}",
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Debug Jest Tests",
-						-- trace = true, -- include debugger info
-						runtimeExecutable = "node",
-						runtimeArgs = {
-							"./node_modules/jest/bin/jest.js",
-							"--runInBand",
-						},
-						rootPath = "${workspaceFolder}",
-						cwd = "${workspaceFolder}",
-						console = "integratedTerminal",
-						internalConsoleOptions = "neverOpen",
-					},
-					{
-						type = "pwa-node",
-						request = "launch",
-						name = "Debug Mocha Tests",
-						-- trace = true, -- include debugger info
-						runtimeExecutable = "node",
-						runtimeArgs = {
-							"./node_modules/mocha/bin/mocha.js",
-						},
-						rootPath = "${workspaceFolder}",
-						cwd = "${workspaceFolder}",
-						console = "integratedTerminal",
-						internalConsoleOptions = "neverOpen",
-					},
-				},
-			}
-		end
-	end,
+    require("dap-vscode-js").setup({
+      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+      -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+      -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+      -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+      -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    })
+
+    for _, language in ipairs({ "typescript", "javascript" }) do
+      require("dap").configurations[language] = {
+        {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require("dap.utils").pick_process,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Jest Tests",
+            -- trace = true, -- include debugger info
+            runtimeExecutable = "node",
+            runtimeArgs = {
+              "./node_modules/jest/bin/jest.js",
+              "--runInBand",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Mocha Tests",
+            -- trace = true, -- include debugger info
+            runtimeExecutable = "node",
+            runtimeArgs = {
+              "./node_modules/mocha/bin/mocha.js",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+        },
+      }
+    end
+  end,
 }
