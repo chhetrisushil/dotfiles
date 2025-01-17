3318a3cbbc34a8e66b79e8d528ac70596d6ca6c7
Sushil Chhetri <chhetrisushil@gmail.com>
2025-01-17 12:20:20 +0530

    nvim: dap config for nodejs app debugging

--- .config/nvim/lua/plugins/dap-config.lua
+++ .config/nvim/lua/plugins/dap-config.lua
@@ -1,127 +1,197 @@
+local js_based_languages = {
+	"typescript",
+	"javascript",
+	"typescriptreact",
+	"javascriptreact",
+	"vue",
+}
+
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "mxsdev/nvim-dap-vscode-js",
-    "leoluz/nvim-dap-go",
-    "theHamsta/nvim-dap-virtual-text",
-    "nvim-neotest/nvim-nio",
-    {
-      "microsoft/vscode-js-debug",
-      opt = true,
-      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-    },
-  },
-  config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    local wk = require("which-key")
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+		"theHamsta/nvim-dap-virtual-text",
+		"nvim-neotest/nvim-nio",
+		{
+			"microsoft/vscode-js-debug",
+			opt = true,
+			run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+		},
+		{
+			"mxsdev/nvim-dap-vscode-js",
+			config = function()
+				---@diagnostic disable-next-line: missing-fields
+				require("dap-vscode-js").setup({
+					-- Path of node executable. Defaults to $NODE_PATH, and then "node"
+					-- node_path = "node",
 
-    -- breakpoint icons and colors
-    vim.cmd("hi DapColor guifg=#fa4848")
-    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapColor", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapBreakpointCondition", { text = "c", texthl = "DapColor", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapColor", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapBreakpointRejected", { text = "R", texthl = "DapColor", linehl = "", numhl = "" })
-    vim.fn.sign_define("DapLogPoint", { text = "L", texthl = "DapColor", linehl = "", numhl = "" })
+					-- Path to vscode-js-debug installation.
+					debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
 
-    -- Setup
-    dapui.setup()
-    require("dap-go").setup()
-    require("nvim-dap-virtual-text").setup()
+					-- Command to use to launch the debug server. Takes precedence over "node_path" and "debugger_path"
+					-- debugger_cmd = { "js-debug-adapter" },
 
-    -- Listeners
-    dap.listeners.before.attach.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close()
-    end
+					-- which adapters to register in nvim-dap
+					adapters = {
+						"chrome",
+						"pwa-node",
+						"pwa-chrome",
+						"pwa-msedge",
+						"pwa-extensionHost",
+						"node-terminal",
+					},
 
-    -- Keymaps
-    wk.add({
-      { "<leader>dt", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
-      {
-        "<leader>dT",
-        function()
-          dap.toggle_breakpoint(vim.fn.input("Breakpoint condition: "))
-        end,
-        desc = "Toggle Conditional Breakpoint",
-      },
-      { "<leader>dc", dap.continue,          desc = "Continue" },
-      {
-        "<leader>d?",
-        function()
-          dapui.eval(nil, { enter = true })
-        end,
-        desc = "Eval",
-      },
-    })
+					-- Path for file logging
+					-- log_file_path = "(stdpath cache)/dap_vscode_js.log",
 
-    require("dap-vscode-js").setup({
-      -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-      -- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
-      -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-      -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-      -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-      -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-    })
+					-- Logging level for output to file. Set to false to disable logging.
+					-- log_file_level = false,
 
-    for _, language in ipairs({ "typescript", "javascript" }) do
-      require("dap").configurations[language] = {
-        {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require("dap.utils").pick_process,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Debug Jest Tests",
-            -- trace = true, -- include debugger info
-            runtimeExecutable = "node",
-            runtimeArgs = {
-              "./node_modules/jest/bin/jest.js",
-              "--runInBand",
-            },
-            rootPath = "${workspaceFolder}",
-            cwd = "${workspaceFolder}",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-          },
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Debug Mocha Tests",
-            -- trace = true, -- include debugger info
-            runtimeExecutable = "node",
-            runtimeArgs = {
-              "./node_modules/mocha/bin/mocha.js",
-            },
-            rootPath = "${workspaceFolder}",
-            cwd = "${workspaceFolder}",
-            console = "integratedTerminal",
-            internalConsoleOptions = "neverOpen",
-          },
-        },
-      }
-    end
-  end,
+					-- Logging level for output to console. Set to false to disable console output.
+					-- log_console_level = vim.log.levels.ERROR,
+				})
+			end,
+		},
+		{
+			"Joakker/lua-json5",
+			build = "./install.sh",
+		},
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
+		local wk = require("which-key")
+
+		-- setup language
+		for _, language in ipairs(js_based_languages) do
+			dap.configurations[language] = {
+				-- Debug single nodejs files
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = vim.fn.getcwd(),
+					sourceMaps = true,
+				},
+				-- Debug nodejs processes (make sure to add --inspect when you run the process)
+				{
+					type = "pwa-node",
+					request = "attach",
+					name = "Attach",
+					processId = require("dap.utils").pick_process,
+					cwd = vim.fn.getcwd(),
+					sourceMaps = true,
+				},
+				-- Debug web applications (client side)
+				{
+					type = "pwa-chrome",
+					request = "launch",
+					name = "Launch & Debug Chrome",
+					url = function()
+						local co = coroutine.running()
+						return coroutine.create(function()
+							vim.ui.input({
+								prompt = "Enter URL: ",
+								default = "http://localhost:3000",
+							}, function(url)
+								if url == nil or url == "" then
+									return
+								else
+									coroutine.resume(co, url)
+								end
+							end)
+						end)
+					end,
+					webRoot = vim.fn.getcwd(),
+					protocol = "inspector",
+					sourceMaps = true,
+					userDataDir = false,
+				},
+				-- Divider for the launch.json derived configs
+				{
+					name = "----- ↓ launch.json configs ↓ -----",
+					type = "",
+					request = "launch",
+				},
+			}
+		end
+
+		-- breakpoint icons and colors
+		vim.cmd("hi DapColor guifg=#fa4848")
+		vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapColor", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointCondition", { text = "c", texthl = "DapColor", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapStopped", { text = "→", texthl = "DapColor", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapBreakpointRejected", { text = "R", texthl = "DapColor", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapLogPoint", { text = "L", texthl = "DapColor", linehl = "", numhl = "" })
+
+		-- Setup
+		dapui.setup()
+		require("dap-go").setup()
+		require("nvim-dap-virtual-text").setup()
+
+		-- Listeners
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- Keymaps
+		wk.add({
+			{ "<leader>dt", dap.toggle_breakpoint, desc = "Toggle Breakpoint" },
+			{
+				"<leader>dT",
+				function()
+					dap.toggle_breakpoint(vim.fn.input("Breakpoint condition: "))
+				end,
+				desc = "Toggle Conditional Breakpoint",
+			},
+			{ "<leader>dc", dap.continue, desc = "Continue" },
+			{
+				"<leader>d?",
+				function()
+					dapui.eval(nil, { enter = true })
+				end,
+				desc = "Eval",
+			},
+			{
+				"<leader>da",
+				function()
+					if vim.fn.filereadable(".vscode/launch.json") then
+						local dap_vscode = require("dap.ext.vscode")
+						dap_vscode.load_launchjs(nil, {
+							["pwa-node"] = js_based_languages,
+							["chrome"] = js_based_languages,
+							["pwa-chrome"] = js_based_languages,
+						})
+					end
+					require("dap").continue()
+				end,
+				desc = "Run with Args",
+			},
+			{
+				"<leader>dO",
+				function()
+					require("dap").step_out()
+				end,
+				desc = "Step Out",
+			},
+			{
+				"<leader>do",
+				function()
+					require("dap").step_over()
+				end,
+				desc = "Step Over",
+			},
+		})
+	end,
 }
