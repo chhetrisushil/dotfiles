ce90655b45f871ab9bf666a755322aaae0448c1e
Sushil Chhetri <chhetrisushil@gmailcom>
2025-08-09 15:31:00 +0530

    fix(fixing the position of the chat window): Fixing position of the chat window to right.

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,10 @@
 return {
   "olimorris/codecompanion.nvim",
   event = "VeryLazy",
-  opts = {},
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "franco-ruggeri/codecompanion-spinner.nvim",
     "github/copilot.vim",
     {
       "ravitemer/mcphub.nvim",
@@ -17,6 +17,14 @@ return {
   },
   config = function()
     require("codecompanion").setup({
+      display = {
+        chat = {
+          window = {
+            position = "right",
+            width = 0.3,
+          },
+        },
+      },
       extensions = {
         mcphub = {
           callback = "mcphub.extensions.codecompanion",
@@ -26,6 +34,7 @@ return {
             show_result_in_chat = true,
           },
         },
+        spinner = { enabled = true },
       },
     })
   end,
